d2b305a7cc4ee268a3fe834b985d9b46e0871b35
Ferrandiz93 <ferrandiz1993@gmail.com>
2016-12-20 19:41:51 +0100

    13 y 14. Impulsor y rotacion nave
    para la rotacion no he usado la funcion que se pedía

---
+++ Examen_DEMI/Assets/Scripts/impulso_nave.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class impulso_nave : MonoBehaviour {
+	public float velocidad = 75f;
+	public float giro = 1f;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			Debug.Log ("Espacio Pulsado");
+			GetComponent<Rigidbody2D> ().AddForce (Vector2.up*velocidad);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate(Vector3.forward * giro); 
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate(Vector3.back * giro); 
+		}
+	}
+}
