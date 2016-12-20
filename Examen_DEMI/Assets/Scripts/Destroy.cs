fda899c23ea5fbde94dc6cec98e8b45834d38177
Ferrandiz93 <ferrandiz1993@gmail.com>
2016-12-20 19:33:08 +0100

    10. Nave
    desaparece si la tocas tambien por detras

---
+++ Examen_DEMI/Assets/Scripts/Destroy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "piedra") {
+			Destroy (gameObject);
+
+		}
+	}
+}
